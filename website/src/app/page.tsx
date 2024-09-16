b08e34ec84eca8fd58a0e7240aec90c2eb7aa35c
takuma39 <takuma.sato.job@gmail.com>
2024-09-16 17:05:57 +0900

    fix：tailwind削除

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -2,14 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <section className="relative flex items-center justify-center bg-black bg-opacity-50 text-white overflow-hidden py-[200px]">
-      <Image
-        className="absolute top-0 right-0 w-full h-[600px] object-cover object-right flex items-center justify-center -z-10"
-        src="/img-mv.jpg"
-        alt=""
-        width={4000}
-        height={1200}
-      />
+    <section className="">
+      <Image className="" src="/img-mv.jpg" alt="" width={4000} height={1200} />
     </section>
   );
 }
