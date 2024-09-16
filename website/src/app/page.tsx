c194f98b006f59eef5a6edf336b6b833d55e588c
takuma39 <takuma.sato.job@gmail.com>
2024-09-16 17:09:11 +0900

    fix：CSS modulesに変更

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,9 +1,16 @@
+import styles from "@/app/page.module.css";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <section className="">
-      <Image className="" src="/img-mv.jpg" alt="" width={4000} height={1200} />
+    <section className={styles.top}>
+      <Image
+        className={styles.bgimg}
+        src="/img-mv.jpg"
+        alt=""
+        width={4000}
+        height={1200}
+      />
     </section>
   );
 }
