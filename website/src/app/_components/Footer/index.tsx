b941acab95be6d7ea0402bace51388f88e75c6af
takuma39 <takuma.sato.job@gmail.com>
2024-10-14 20:36:11 +0900

    add：3-4-2：footer追加

---
+++ website/src/app/_components/Footer/index.tsx
@@ -0,0 +1,25 @@
+import styles from "./index.module.css";
+import Image from "next/image";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <nav className={styles.nav}>
+        <ul className={styles.items}>
+          <li className={styles.item}>
+            <a href="/news">ニュース</a>
+          </li>
+          <li className={styles.item}>
+            <a href="/members">メンバー</a>
+          </li>
+          <li className={styles.item}>
+            <a href="/contact">お問い合わせ</a>
+          </li>
+        </ul>
+      </nav>
+      <p className={styles.cr}>© SIMPLE. All Rights Reserved 2024</p>
+    </footer>
+  );
+};
+
+export default Footer;
