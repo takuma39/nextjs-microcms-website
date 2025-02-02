3eb3feac4c9718256c994f566df4c47ddce430d9
takuma39 <takuma.sato.job@gmail.com>
2025-02-02 18:17:33 +0900

    add：6

---
+++ website/src/app/news/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import { getNewsList } from "@/app/_libs/microcms";
+import NewsList from "@/app/_components/NewsList";
+
+export default async function Page() {
+  const { contents: news } = await getNewsList();
+  return <NewsList news={news} />;
+}
