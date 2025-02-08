84558a501aaed406ce6f1b7af626ee204ac70206
takuma39 <takuma.sato.job@gmail.com>
2025-02-08 16:29:19 +0900

    add：7

---
+++ website/src/app/_components/Pagination/index.tsx
@@ -0,0 +1,38 @@
+import { NEWS_LIST_LIMIT } from "@/app/_constants";
+import Link from "next/link";
+import styles from "./index.module.css";
+import { current } from "@reduxjs/toolkit";
+
+type Props = {
+  totalCount: number;
+  current?: number;
+  basePath?: string;
+};
+
+export default function Pagination({ 
+  totalCount,
+  current = 1,
+  basePath = "/news",
+ }: Props) {
+  const pages = Array.from(
+    { length: Math.ceil(totalCount / NEWS_LIST_LIMIT) },
+    (_, i) => i + 1
+  );
+  return (
+    <nav>
+      <ul className={styles.container}>
+        {pages.map((page) => (
+          <li className={styles.list} key={page}>
+            {current !== page ? (
+              <Link href={`${basePath}/p/${page}`} className={styles.item}>
+                {page}
+              </Link>
+            ) : (
+              <span className={`${styles.item} ${styles.current}`}>{page}</span>
+            )}
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
