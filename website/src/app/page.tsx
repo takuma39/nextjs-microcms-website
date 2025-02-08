323cbcf3f065ec73a7c756076444e52903efa1bc
takuma39 <takuma.sato.job@gmail.com>
2025-02-09 00:59:44 +0900

    fix：サイト全体にrevalidateを設定

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -5,6 +5,8 @@ import { NEWS_LIST_LIMIT } from "@/app/_constants";
 import ButtonLink from "@/app/_components/ButtonLink";
 import NewsList from "@/app/_components/NewsList";
 
+export const revalidate = 60;
+
 export default async function Home() {
   const data = await getNewsList({ limit: NEWS_LIST_LIMIT });
   return (
