68c012d9121beebc34c872a509cbb75ef247b03c
takuma39 <takuma.sato.job@gmail.com>
2025-02-10 20:16:29 +0900

    fix：hero画像を修正

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -24,6 +24,8 @@ export default async function Home() {
           alt=""
           width={4000}
           height={1200}
+          priority
+          sizes="100vw"
         />
       </section>
 
